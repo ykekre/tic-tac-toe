76a10b8f79a7b04b2bbe22cbd9a9ac63ee3c174a
unknown <kekre.dev@gmail.com>
2019-03-11 13:15:51 +0530

    add favicons, code refactor, add meta description tag in html

--- app.js
+++ app.js
@@ -2,68 +2,67 @@
 let humanType = 'x',
     botType = 'o',
     gameBegun = 0,
-    player = ['bot', 'human'],
-    playerTurn = true,
-    availableCells = [],
-    xscore = 0;
+    isHumanTurn = true,
+    humanCells = [], botCells = [], availableCells = [],
+    xscore = 0,
     oscore = 0;
-    humanCells = [];
-    botCells = [];
 
 const xMarkup = `<div class="ex">
                         X
-                  </div>`;
+                  </div>`,
 
-const oMarkup = `<div class="ohh">
+      oMarkup = `<div class="ohh">
                   O
-                </div>`;
+                </div>`,
 
-const elements = {
+      elements = {
+                  'cell0': document.querySelector('.cell-0'),
+                  'cell1': document.querySelector('.cell-1'),
+                  'cell2': document.querySelector('.cell-2'),
+                  'cell3': document.querySelector('.cell-3'),
+                  'cell4': document.querySelector('.cell-4'),
+                  'cell5': document.querySelector('.cell-5'),
+                  'cell6': document.querySelector('.cell-6'),
+                  'cell7': document.querySelector('.cell-7'),
+                  'cell8': document.querySelector('.cell-8'),
+                  'btnEx': document.querySelector('.btn--ex'),
+                  'btnOhh': document.querySelector('.btn--ohh'),
+                  'grid': document.querySelector('div.grid'),
+                  'winner': document.querySelector('.winner'),
+                  'result-h1': document.querySelector('.winner__header'),
+                  'win-o': document.querySelector('.winner .js--o'),
+                  'win-x': document.querySelector('.winner .js--x'),
+                  'xscore': document.querySelector('.xScore'),
+                  'oscore': document.querySelector('.oScore'),
+                  'restart': document.querySelector('.btn--restart'),
+                  'msg': document.querySelector('.header__display--msg')
+                },
 
-  'cell0': document.querySelector('.cell-0'),
-  'cell1': document.querySelector('.cell-1'),
-  'cell2': document.querySelector('.cell-2'),
-  'cell3': document.querySelector('.cell-3'),
-  'cell4': document.querySelector('.cell-4'),
-  'cell5': document.querySelector('.cell-5'),
-  'cell6': document.querySelector('.cell-6'),
-  'cell7': document.querySelector('.cell-7'),
-  'cell8': document.querySelector('.cell-8'),
-  'btnEx': document.querySelector('.btn--ex'),
-  'btnOhh': document.querySelector('.btn--ohh'),
-  'grid': document.querySelector('div.grid'),
-  'winner': document.querySelector('.winner'),
-  'result-h1': document.querySelector('.winner__header'),
-  'win-o': document.querySelector('.winner .js--o'),
-  'win-x': document.querySelector('.winner .js--x'),
-  'xscore': document.querySelector('.xScore'),
-  'oscore': document.querySelector('.oScore'),
-  'restart': document.querySelector('.btn--restart'),
-  'msg': document.querySelector('.header__display--msg')
-}
+      grid = [
+              elements.cell0,
+              elements.cell1,
+              elements.cell2,
+              elements.cell3,
+              elements.cell4,
+              elements.cell5,
+              elements.cell6,
+              elements.cell7,
+              elements.cell8
+            ],
 
-const grid = [
-  elements.cell0,
-  elements.cell1,
-  elements.cell2,
-  elements.cell3,
-  elements.cell4,
-  elements.cell5,
-  elements.cell6,
-  elements.cell7,
-  elements.cell8
-]
+      winCombos = [
+                    ['cell-0', 'cell-1', 'cell-2'],
+                    ['cell-3', 'cell-4', 'cell-5'],
+                    ['cell-6', 'cell-7', 'cell-8'],
+                    ['cell-0', 'cell-3', 'cell-6'],
+                    ['cell-1', 'cell-4', 'cell-7'],
+                    ['cell-2', 'cell-5', 'cell-8'],
+                    ['cell-2', 'cell-4', 'cell-6'],
+                    ['cell-0', 'cell-4', 'cell-8']
+                  ],
+      player = ['bot', 'human'],
+      result = ['win', 'draw']
 
-const winCombos = [
-  ['cell-0', 'cell-1', 'cell-2'],
-  ['cell-3', 'cell-4', 'cell-5'],
-  ['cell-6', 'cell-7', 'cell-8'],
-  ['cell-0', 'cell-3', 'cell-6'],
-  ['cell-1', 'cell-4', 'cell-7'],
-  ['cell-2', 'cell-5', 'cell-8'],
-  ['cell-2', 'cell-4', 'cell-6'],
-  ['cell-0', 'cell-4', 'cell-8']
-]
 
 //? Initialize stuff in beginning like playertype and setup grid listeners
 function init() {
@@ -71,14 +70,21 @@ function init() {
   //* 1. Setup grid listeners
     setupListeners();
 
-
-  //* 2. check if player has started game
+  //* 2. check if human has started game
     elements.grid.addEventListener('click', () => {
       gameBegun = 1;
     });
 
-  //* 3. //? Initially populate available cells array with all cells of grid. We will remove cells from this array as the game moves on and players click on the cells
+  /*
+   * 3. Initially populate available cells array with all cells of grid.
+        *We will remove cells from this array as the game moves on
+        *and players click on the cells
+  */
     fillAvailableCells();
+
+  //*4. Hide the restart button
+
+  elements.restart.classList.add('hide');
 }
 
 //? Call init
@@ -94,39 +100,45 @@ function fillAvailableCells() {
 //? Setup all listeners
 function setupListeners() {
 
-  //* 1. GRID Listener: to pickup cells which have been clicked by player
+  //* 1. GRID Listener: to pickup cells which have been clicked by human
     for (const cell of grid) {
-
       cell.addEventListener('click', (e) => {
         //! Which cell user has clicked on
         cellTarget = e.srcElement.classList[1];
 
         //!Call player move fn with this cell
          if(availableCells.includes(cellTarget)) {
-           humanTurn(cellTarget);
+           HumanTurn(cellTarget);
         }
+
+        //!4. Show the restart button
+        elements.restart.classList.remove('hide');
       })
     }
 
   //* 2. Score Card Listeners
-    //? Left-score-card button
-    elements["btnEx"].addEventListener('click', () => {
-      //! if use has not started game already allow him to pick his choice of weapon
-      if (gameBegun === 0) {
-        pickWeapon("btnEx", 'btnOhh');
-      }
-    })
+    //? Left-score-card button listener
+    scoreButtonListener('btnEx');
 
-    //? Right-score-card button
-    elements["btnOhh"].addEventListener('click', () => {
-      //! if use has not started game already allow him to pick his choice of weapon
-      if (gameBegun === 0) {
-        pickWeapon("btnOhh", 'btnEx');
-      }
-    })
+    //? Right-score-card button listener
+    scoreButtonListener('btnOhh');
 
   //* 3. Restart button listener
     elements.restart.addEventListener('click', restartGame);
+}
+
+
+//? Score display button listeners,
+//?Also used to select player type (X or O) in beginning of game
+function scoreButtonListener(button) {
+  elements[button].addEventListener('click', () => {
+    //! if use has not started game already allow him to pick his choice of weapon
+    if (gameBegun === 0) {
+      if (button === 'btnEx' ?  pickWeapon("btnEx", 'btnOhh') : pickWeapon("btnOhh", 'btnEx') ) ;
+    }
+
+    display("Start game by making first move")
+  });
 }
 
 //? DETERMINE PLAYER TYPE
@@ -149,33 +161,16 @@ function pickWeapon(type, inverseType) {
 }
 
 //? Human makes a move
-function humanTurn(cell) {
+function HumanTurn(cell) {
 
-  //* 1. Add X's or O's to grid as per human type
-  if (availableCells.includes(cell) && playerTurn && !humanCells.includes(cell) ) {
-
+  //* 1. Add X's or O's to grid as per player's weapon type
+  if (availableCells.includes(cell) && isHumanTurn && !humanCells.includes(cell) ) {
     renderXorO(cell, humanType);
     humanCells.push(cell);
   }
 
-  //* unless there is already a winner call bot to play
-    if ( !isWinning(humanCells) ) {
-
-      if(availableCells.length > 0) {
-
-        playerTurn = false;
-        display(`${botType}'s turn`);
-
-        //* Call bot to make a move next with botTurnSmart
-        setTimeout(botTurnSmart ,500) ;
-
-      } else {
-          onDraw();
-        }
-
-    } else {
-      onWin(player[1]);
-    }
+  //* 2. Next move
+  nextStep(humanCells, player[1]);
 }
 
 //? Make X's or O's Characters visible on grid as per player type when they make a move
@@ -197,109 +192,151 @@ function renderXorO(cell, type) {
 
 }
 
+//? fn to call after player has made a move
+function nextStep(playerCells, who) {
 
-//? This is called to make bot play an 'intelligent' move: It either tries for a win in the next move - if available or tries to block opponent's win in next move.
-//? If either of above conditions are NA, it calls the botTurn fn to make a random move from amongst the available cells
+  //* unless this move is winning, call next player to play
+  if (!isWinning(playerCells)) {
+
+    //!If there are still available cells on grid
+    if (availableCells.length > 0) {
+
+      //!Check if player is bot
+      if (who === player[0]) {
+        isHumanTurn = true ;
+        display(`${humanType}'s turn`);
+
+      }
+
+      //!player is human
+      else {
+        isHumanTurn = false;
+        display(`${botType}'s turn`);
+
+        //! Call bot to make a move next with botTurnSmart
+          setTimeout(botTurnSmart ,500) ;
+      }
+    }
+
+    //! not winning and no more free cells - hence draw
+    else {
+      gameResult(result[1], who);
+    }
+  }
+
+  //* winning with this move
+  else { gameResult(result[0], who);
+  }
+
+}
+
+/*
+  ?Below fn is called to make bot play an 'intelligent' move i.e:
+  ?It either tries for a win in the next move - if available
+  ?or tries to block opponent's win in next move.
+  ?If either of above conditions are NA,
+  ?it calls the botTurnDumb fn to make a random move from amongst the available cells
+*/
 
 function botTurnSmart () {
 
-  if(availableCells.length === 0) {
-    onDraw();
-  }
-  //? Steps to take
-    //* 1. take each element from available cells arr, push it to botCells array clone.
-    //* 2. compare this new arr with each wincombo - use checkWinner function
-        //! if winning - play the move with this cell
-        //! if not winning with any combo, break out of this loop and try with next element in aviailble cells
-    //* 3. Next try above with humanCells combo
-    //*4. If still nothing, Play some random move with the botTurn fn
+  /*
+    ? Steps to take
+    * 1. take each element from available cells arr,
+        *push it to a botCells array clone.
+    * 2. check if this clone has any of the winning combos present
+        ! if winning - play the move with this cell
+        ! if not winning with any combo, break out of this loop and
+        !try above next element in available cells
+    * 3.Next try above with humanCells combo
+    *4. If still nothing, Play some random move with the botTurnDumb fn
+  */
 
-        if (bothelper(botCells.slice())) {
+        if (botHelper(botCells.slice())) {
           return;
-        } else if  ( bothelper(humanCells.slice())) {
+        } else if  ( botHelper(humanCells.slice())) {
           return;
         }
         else {
           botTurnDumb();
         }
 
-    }
+        function botHelper ( arr ) {
 
-    function bothelper ( arr ) {
-      for (let i = 0; i < availableCells.length; i++) {
-        const testCell = availableCells[i];
-        arr.push(testCell);
+          //* take each cell from available cells, push it to playerCells array
+          for (let i = 0; i < availableCells.length; i++) {
+            const testCell = availableCells[i];
+            arr.push(testCell);
 
-          if ( isWinning(arr) ) {
-            botMove(testCell);
-            return true;
-          } else {
-            arr.pop();
-            continue;
+            //*test if this new array has any winning combo
+              if ( isWinning(arr) ) {
+                //* if true, play botMove with this cell
+                botMove(testCell);
+
+                //* return true to botTurnSmart
+                return true;
+              } else {
+                //* reject this cell, try with next avaialble cell
+                arr.pop();
+                continue;
+              }
           }
-      }
+        }
+
+        //? This fn is called when we can't determine a forcing move for the bot i.e
+        //? a move which either gives a win to the bot or prevents the human from winning.
+        //?In which case a random move is played by bot
+          //! THIS IS TO BE CALLED AS A LAST RESORT
+          //TODO: MAKE RANDOM CHOOSING - ALSO SMARTER
+
+        function botTurnDumb() {
+          //* Randomly pick a cell for bot from available cells
+            const totalCells = availableCells.length;
+
+            const pickNumber = Math.floor(Math.random() * totalCells);
+
+            const pickCell = availableCells[pickNumber];
+
+          //* 1. Add X's or O's to grid as per bot type ON chosen cell
+            botMove(pickCell);
+        }
+
+        //? 1. Add X's or O's to grid as per bot type ON chosen cell
+        function botMove(pickCell) {
+
+          if (availableCells.includes(pickCell) && !isHumanTurn && !botCells.includes(pickCell) && !humanCells.includes(pickCell)) {
+            renderXorO(pickCell, botType);
+
+            //* Add this cell to array of cells on which bot has made a move
+            botCells.push(pickCell);
+          }
+
+          //* next check for win or draw
+          //* else call bot to play
+            nextStep(botCells, player[0]);
+        }
     }
 
 
 
-//? 1. Add X's or O's to grid as per bot type ON chosen cell
-function botMove(pickCell) {
 
-  if (availableCells.includes(pickCell) && !playerTurn && !botCells.includes(pickCell) && !humanCells.includes(pickCell)) {
-    renderXorO(pickCell, botType);
-
-    //* Add this cell to array of cells on which bot has made a move
-    botCells.push(pickCell);
-  }
-
-  //* check for win
-    if ( !isWinning(botCells) ) {
-
-      if(availableCells.length > 0) {
-
-        playerTurn = true;
-        display(`${humanType}'s turn`);
-      }
-      else {
-        onDraw();
-      }
-
-    } else {
-      onWin(player[0]);
-    }
-}
-
-
-//? This is called when we can't determine a forcing move for the bot i.e a move which either gives a win to the bot or prevents the human from winning. In which case a random move is played by bot
-  //! THIS IS TO BE CALLED AS A LAST RESORT
-  /*
-  TODO: MAKE RANDOM CHOOSING - ALSO SMARTER
-  */
-function botTurnDumb() {
-  //* Randomly pick a cell for bot from available cells
-    const totalCells = availableCells.length;
-
-    const pickNumber = Math.floor(Math.random() * totalCells);
-
-    const pickCell = availableCells[pickNumber];
-
-  //* 1. Add X's or O's to grid as per bot type ON chosen cell
-    botMove(pickCell);
-}
-
-
+//? Fn to check whether the given array of cells has a winning combo
 function isWinning(playerCells) {
-  //* compare each win combo with playercells
 
+  //* compare each win combo with playercells
   for (let index = 0; index < winCombos.length; index++) {
     const combo = winCombos[index];
+
+    //! winCount keeps track of no. of cells from playerCells arr
+    //! which were found in any given winning combo
+    //! when it reaches 3, it means we have found a winning combo and can return back
     let winCount = 0;
-    let winnArr = [];
+    ////let winnArr = [];
     for (let c = 0; c < combo.length; c++) {
       for (let i = 0; i < playerCells.length; i++) {
         if (combo[c] === playerCells[i]) {
           winCount++;
-          winnArr.push(playerCells[i]);
+          ////winnArr.push(playerCells[i]);
           if (winCount === 3) {
             return true;
           }
@@ -310,52 +347,47 @@ function isWinning(playerCells) {
     }
 
   }
-
   return false;
 }
 
 //? Things to do when a winner has been found
-function onWin (winner) {
+function gameResult (result, who) {
 
   //* 1. fade the tic-tac-toe grid and its contents and display the winner card
     elements.winner.classList.add('show');
     elements.grid.classList.add('hide');
 
-  //* 2. Update winner scores and show the winner weapon type on winner card
-    if(winner === player[0]) {
+    if(result === 'win') {
+      //* 2. Update winner scores and show the winner weapon type on winner card
+      if(who === player[0]) {
+        elements[`win-${humanType}`].classList.add('hide');
+
+        if(botType === 'x'? xscore++ : oscore++);
+
+      } else {
+        elements[`win-${botType}`].classList.add('hide');
+        if(humanType === 'x'? xscore++ : oscore++);
+      }
+
+      //*3. Change header display
+      display(`Game won by ${who}`);
+
+      //* 4. Display result text
+      elements["result-h1"].textContent = "WINNER";
+    }
+    else {
+
+      //*2. Show both weapon types on winner card
+      elements[`win-${botType}`].classList.add('hide');
       elements[`win-${humanType}`].classList.add('hide');
 
-      if(botType === 'x'? xscore++ : oscore++);
+      //*3. Change header display
+      display('Game Drawn');
 
-    } else {
-      elements[`win-${botType}`].classList.add('hide');
-      if(humanType === 'x'? xscore++ : oscore++);
+      //* 4. Display result text
+      elements["result-h1"].textContent = "DRAW";
     }
-
-  //*3. Change header display
-    display('Game Over');
-
-  //* 4. Display result text
-    elements["result-h1"].textContent = "WINNER";
-}
-
-//? Call on game Drawn
-function onDraw() {
-
-  //* 1. fade the tic-tac-toe grid and its contents and display the winner card
-    elements.winner.classList.add('show');
-    elements.grid.classList.add('hide');
-
-    elements[`win-${botType}`].classList.add('hide');
-    elements[`win-${humanType}`].classList.add('hide');
-
-  //*2. Change header display
-    display('Game Drawn');
-
-  //* 4. Display result text
-    elements["result-h1"].textContent = "DRAW";
-}
-
+  }
 
 //? Displays the player scores and if a message has to be displayed to user
 function display(message) {
@@ -375,6 +407,7 @@ function  restartGame() {
   //* 2 Empty player cells arrays
     humanCells = [];
     botCells = [] ;
+    availableCells = [];
 
   //*3 Refill available cells array
     fillAvailableCells();
@@ -402,6 +435,9 @@ function  restartGame() {
   elements["result-h1"].textContent = "";
 
   //*8 Set player turn to true
-  playerTurn = true;
+  isHumanTurn = true;
+
+   //*9. Hide the restart button
+   elements.restart.classList.add('hide');
 }
 
